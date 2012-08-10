bb4b3df7a28715dd8859fe158ca611337a073121
Steve Ivy <steve@wallrazer.com>
2012-08-10 16:57:18 -0700

    use jinja templates in views, and other tweaks
    * updates to views
    * add requirements file
    * add logged in user section to template
    * tests!

--- main.py
+++ main.py
@@ -15,20 +15,76 @@
 # limitations under the License.
 #
 import webapp2
+# import cgi
+import urllib
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname('__file__')
+        + './templates'))
 
 from google.appengine.api import users
+from google.appengine.ext import db
+
+
+class Greeting(db.Model):
+    author = db.StringProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+
+
+def guestbook_key(guestbook_name=None):
+    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+
+        guestbook_name = self.request.get('guestbook_name')  # or default_guestbook
+
+        # SELECT * FROM is a given when used in Model context
+        greetings = Greeting.gql("WHERE ANCESTOR IS :1 "
+                                "ORDER BY date DESC LIMIT 10",
+                                guestbook_key(guestbook_name))
+
         user = users.get_current_user()
-
+        user_nick = ''
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello %s!' % user.nickname())
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            user_nick = user.nickname()
         else:
-            self.redirect(users.create_login_url(self.request.uri))
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'guestbook_name': guestbook_name,
+            'greetings': greetings,
+            'user_nick': user_nick,
+            'url_linktext': url_linktext,
+            'url': url
+        }
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
 
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        guestbook_name = self.request.get('guestbook_name')
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        print "greeting: "
+        if users.get_current_user():
+            greeting.author = users.get_current_user().nickname()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        # redirect to home page
+        self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                              ('/sign', Guestbook)],
                               debug=True)
--- tests/views.py
+++ tests/views.py
@@ -1,24 +1,47 @@
+import os
+
 from webtest import TestApp
 from main import app, Greeting, guestbook_key
 from nose.tools import eq_, ok_
 
-# from google.appengine.ext import testbed
+from google.appengine.ext import testbed
 
 
 class TestIndex(object):
 
     def setUp(self):
         self.testapp = TestApp(app)
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_user_stub()
 
     def test_index(self):
         response = self.testapp.get('/')
         assert 'Guestbook' in str(response)
+
+    def test_loggedin_index(self):
+        os.environ['USER_EMAIL'] = 'testuser@example.com'
+        os.environ['USER_ID'] = '123'
+        os.environ['AUTH_DOMAIN'] = 'testbed'
+
+        response = self.testapp.get('/')
+
+        ok_('Logout' in str(response))
+        ok_('testuser' in str(response))
+
+    def tearDown(self):
+        self.testbed.deactivate()
 
 
 class TestGreetings(object):
 
     def setUp(self):
         self.testapp = TestApp(app)
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_user_stub()
 
     def test_post_greeting(self):
         response = self.testapp.post('/sign', {'guestbook_name': 'test', 'content': 'hi'})
@@ -29,3 +52,16 @@ class TestGreetings(object):
                                   guestbook_key('test'))
 
         ok_(greetings.count() == 1, 'There should be 1 Greeting object')
+
+    def test_loggedin_post_greeting(self):
+        self.testapp.post('/sign', {'guestbook_name': 'test', 'content': 'hi'})
+
+        greetings = Greeting.gql("WHERE ANCESTOR IS :1 "
+                                 "ORDER BY date DESC LIMIT 10",
+                                  guestbook_key('test'))
+        greeting = greetings.fetch(1)[0]  # latest
+
+        ok_(greeting.content == 'hi', 'greeting content should be \'hi\'')
+
+    def tearDown(self):
+        self.testbed.deactivate()
