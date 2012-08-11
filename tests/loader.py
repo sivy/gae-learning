35786748820c39736ee315bfa4bb3d9d742d075c
Steve Ivy <steve@wallrazer.com>
2012-08-11 13:45:18 -0700

    add a few more test assertions

--- tests/loader.py
+++ tests/loader.py
@@ -86,11 +86,13 @@ class TestLoader(object):
         greeting_query = Greeting.gql("WHERE ANCESTOR IS :1 "
                                  "ORDER BY date DESC LIMIT 10",
                                  guestbook_key('test'))
-        greetings = greeting_query.fetch(10)
+        eq_(greeting_query.count(), 2, 'There should be 2 Greetings')
 
+        greetings = greeting_query.fetch(10)
         ok_(greetings, 'Greetings should not be None')
 
-        eq_(type(greetings), list)
+        eq_(type(greetings), list, 'greetings should be a list')
+        eq_(len(greetings), 2, '2 Greetings should be fetched')
 
         greeting1 = greetings[0]
         greeting2 = greetings[1]
